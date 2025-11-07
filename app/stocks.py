1969f70a83ec8de144f8a3823b0d39a0d9e14e46
orm16-debug <orm16@georgetown.edu>
2025-11-07 17:53:49 -0500

    created stock data fetching code (#4)

---
+++ app/stocks.py
@@ -0,0 +1,23 @@
+import os
+from pandas import read_csv
+from dotenv import load_dotenv
+import plotly.express as px
+
+load_dotenv()
+# loads environment variables from the ".env" file and recognizes them
+
+ALPHAVANTAGE_API_KEY = os.getenv("ALPHAVANTAGE_API_KEY")
+
+def fetch_stocks_csv(symbol="NFLX"):
+    request_url = f"https://www.alphavantage.co/query?function=TIME_SERIES_DAILY_ADJUSTED&symbol={symbol}&apikey={ALPHAVANTAGE_API_KEY}&datatype=csv"
+    stocks_df = read_csv(request_url)
+    return stocks_df
+
+if __name__ == "__main__":
+
+    symbol = input("Please choose a stock symbol: ") or "NFLX"
+    stocks_df = fetch_stocks_csv(symbol)
+    print(stocks_df.head())
+
+    fig = px.line(stocks_df, x="timestamp", y="adjusted_close", title=f"Stock Prices ({symbol})", height=500)
+    fig.show()
